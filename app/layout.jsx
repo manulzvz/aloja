582e2d6628dc44437f629c21f359c312ed739a65
manulzvz13 <manulzvz@gmail.com>
2025-08-27 19:09:26 -0600

    feat: Add HomePage and layout

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import "@/assets/styles/globals.css";
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
